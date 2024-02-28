7382295a6954236a16f7dfa2f21e05bf7e563ac3
Hunafarizal <hunafarizal@gmail.com>
2024-02-28 20:01:33 +0700

    submission tugas capstone

--- CC1.py
+++ CC1.py
@@ -11,16 +11,16 @@ database = []
 def load_database():
     global database
     if os.path.exists(database_file_path):
-        with open(database_file_path, 'r') as file:
+        with open(database_file_path, 'r') as file: #r = read (buka file)
             lines = file.readlines()
-            database = [line.strip().split(',') for line in lines]
+            database = [line.strip().split(',') for line in lines] 
             for record in database:
                 record[0] = int(record[0])  # Untuk mengconvert ID siswa menjadi form Integer
                 record[2:5] = map(float, record[2:5]) # untuk mengconvert nilai siswa menjadi form Float
 
 #fungsi agar menyimpan revisi, penghapusan, penambahan, dll pada database
 def save_database():
-    with open(database_file_path, 'w') as file:
+    with open(database_file_path, 'w') as file: #w = write (overwrite/save file)
         for record in database:
             file.write(','.join(map(str, record)) + '\n')
 
@@ -33,34 +33,37 @@ def display_menu():
     print("3. Update Record")
     print("4. Delete Record")
     print("5. Exit")
-
+    print("=====================================================")
 #Fungsi utama untuk function READ
 def read_database():
-    print("\n========Read Database========")
+    print("\n>>>>>>>>>> Read Database <<<<<<<<<<")
     print("1. Display All Records")
     print("2. Search for Specific Data")
     print("3. Back to Main Menu")
-
-    choice = input("Enter your choice: ")
+    print('>>>>>>>>>>>>>>>>>.<<<<<<<<<<<<<<<<<\n')
+    choice = input("Enter your choice (1-3): ")
 
     if choice == '1':
         display_records(database)
     elif choice == '2':
         #Ini adalah fungsi sub-menu jika user hanya ingin mencari 1 data tertentu.
-        #Fungsi validation_positive ini agar ID tidak bisa dimasukkan dengan selain angka integer positif
-        search_id = validation_positive("Enter the Student's ID: ", is_integer=True) 
+        #Fungsi validation_nisn ini agar ID tidak bisa dimasukkan dengan selain dengan kombinasi 10 angka
+        search_id = validation_nisn("Enter the Student's ID: ", is_integer=True) 
         matching_records = [record for record in database if record[0] == int(search_id)]
         display_records(matching_records)
+        return read_database()
     elif choice == '3':
         return
     else:
         print("Error! Please choose a number between 1-3")
+        return read_database()
 
 #Jika tidak ada data sama sekali, maka fungsi akan print string dibawah ini dan kembali ke menu utama
 def display_records(records):
     if not records:
         print("No records found.")
-        return
+        return read_database()
+        
 #kita masukkan plugin prettytable pada output nya
     table = PrettyTable()
     table.field_names = ["Student ID", "Name", "Math Score", "Science Score", "English Score", "Pass Status"]
@@ -76,17 +79,34 @@ def display_records(records):
 
     print(table)
 
+#Submenu menuju fungsi ADD
+def add_submenu() :
+    print("\n++++++++++ Add Record ++++++++++")
+    print("1. Input New Student Record")
+    print("2. Back to main menu")
+    print('++++++++++++++++++++++++++++++++\n')
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == '1' :
+        return add_record()
+    elif choice == '2' :
+        return
+    else :
+        print("Error! Please choose a number between 1-2")
+        return add_submenu()
+    
 #Fungsi utama untuk function ADD
 def add_record():
     print("\n========Add Record========")
-    student_id = validation_positive("Enter Student's ID: ", is_integer=True)
+    student_id = validation_nisn("Enter Student's ID: ", is_integer=True)
+
 
     # Kita cek apakah ID sudah terdaftar atau belum. Jika sudah, user akan ditolak dan diminta memasukkan input baru
     if any(record[0] == student_id for record in database):
         print("Student ID already exists. Cannot add duplicate records.")
-        return
+        return add_submenu()
 
-    name = input("Enter Student's Name: ")
+    name = validation_name("Enter Student's Name: ")
     math_score = validation_score("Enter Math Score: ")
     science_score = validation_score("Enter Science Score: ")
     english_score = validation_score("Enter English Score: ")
@@ -97,32 +117,59 @@ def add_record():
     #konfirmasi display akan menunjukkan yang user baru saja add. Jika sudah cocok, maka user dapat konfirmasikan
     print("\nRecent Changes:")
     display_records([record])
-    confirmation = input("Do you want to add this record? (yes/no): ").lower()
+    confirmation = input("Do you want to add this record? (type yes to confirm): ").lower()
 
     if confirmation == 'yes':
         save_database()
         print("Record added successfully.")
+        return add_submenu()
     else:
         print("Add operation canceled.")
+        return add_submenu()
+    
+#submenu menuju fungsi UPDATE
+def update_submenu() :
+    print("\n^^^^^^^^^^ Update Record ^^^^^^^^^^")
+    print("1. Update Existing Student Record")
+    print("2. Back to main menu")
+    print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n')
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == '1' :
+        return update_record()
+    elif choice == '2' :
+        return
+    else :
+        print("Error! Please choose a number between 1-2")
+        return update_submenu()
+    
 
 #Fungsi utama untuk function UPDATE
 def update_record():
     print("\n========Update Record========")
-    search_id = validation_positive("Enter the Student's ID to update: ", is_integer=True)
+    search_id = validation_nisn("Enter the Student's ID to update: ", is_integer=True)
     matching_records = [record for record in database if record[0] == int(search_id)]
-
 
     if not matching_records:
         print("No record found with the given ID. Please input existing ID")
-        return
+        return update_submenu()
+    
     #Program akan menampilkan terlebih dahulu data yang user ingin update sebelum user memutuskan untuk update apa tidak
     display_records(matching_records)
     #konfirmasi apakah user benar ingin meng-update data tersebut
-    confirmation = input("Do you want to update this record? (yes/no): ").lower()
+    confirmation = input("Do you want to update this record? (type yes to confirm): ").lower()
 
-    if confirmation == 'yes':
-        new_id = validation_positive("Enter new Student's ID: ")
-        new_name = input("Enter new Student's Name: ")
+    if confirmation != 'yes':
+        print('Update operation canceled.')
+        return update_submenu()
+
+    elif confirmation == 'yes':
+        student_id = [record[0] for record in database]
+        new_id = input("Enter new Student's ID: ")
+        if int(new_id) in student_id :
+            print('Cannot update to an already existing ID in the database.')
+            return update_record()
+        new_name = validation_name("Enter new Student's Name: ")
         #validation_score adalah fungsi yang saya gunakan agar user hanya bisa menginput nilai mulai dari 0 - 100
         new_math_score = validation_score("Enter new Math Score: ")
         new_science_score = validation_score("Enter new Science Score: ")
@@ -130,56 +177,75 @@ def update_record():
         new_pass_status = calculate_pass_status(new_math_score, new_science_score, new_english_score)
 
         updated_record = [new_id, new_name, new_math_score, new_science_score, new_english_score, new_pass_status]
+       
 
-    #konfirmasi display akan menunjukkan yang user baru saja add. Jika sudah cocok, maka user dapat konfirmasikan
-    print("\nRecent Changes:")
-    display_records([updated_record])
-    confirmation = input("Do you want to update this record? (yes/no): ").lower()
+    #konfirmasi display akan menunjukkan yang user baru saja add.3
+    #  Jika sudah cocok, maka user dapat konfirmasikan
+        print("\nRecent Changes:")
+        display_records([updated_record])
+    confirmation = input("Do you want to update this record? (type yes to confirm): ").lower()
 
     if confirmation == 'yes':
         database.remove(matching_records[0])
         database.append(updated_record)
         save_database()
         print("Record updated successfully.")
+        return update_submenu()
     else:
         print("Update operation canceled.")
+        return update_submenu()
+
+#submenu menuju fungsi DELETE      
+def delete_submenu() :
+    print("\n-------------Delete Record-------------")
+    print("1. Delete Existing Student Record")
+    print("2. Back to main menu")
+    print('---------------------------------------\n')
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == '1' :
+        return delete_record()
+    elif choice == '2' :
+        return
+    else :
+        print("Error! Please choose a number between 1-2")
+        return delete_submenu()
 
 #Fungsi utama function DELETE
 def delete_record():
     print("\n========Delete Record========")
-    search_id = validation_positive("Enter the Student's ID to delete: ", is_integer=True)
+    search_id = validation_nisn("Enter the Student's ID to delete: ", is_integer=True)
     matching_records = [record for record in database if record[0] == int(search_id)]
 
     if not matching_records:
         print("No record found with the given ID.")
-        return
+        return delete_submenu()
+    
     #ditampilkan data nya terlebih dahulu (sama seperti update tadi)
     display_records(matching_records)
     #lalu konfirmasi
-    confirmation = input("Do you want to delete this record? (yes/no): ").lower()
+    confirmation = input("Do you want to delete this record? (type yes to confirm): ").lower()
 
     if confirmation == 'yes':
         database.remove(matching_records[0])
         print("Record deleted successfully.")
+        return delete_submenu()
     else:
         print("Delete operation canceled.")
+        return delete_submenu()
+    
 
-#Ini adalah penjabaran dari fungsi validasi angka positif tadi yang digunakan diatas pada input ID
-def validation_positive(prompt, is_integer=True):
+#Ini adalah penjabaran dari fungsi validasi ID NISN siswa agar user hanya bisa memasukkan kombinasi 10 angka
+def validation_nisn(prompt, is_integer=True):
     while True:
         try:
-            if is_integer:
-                value = int(input(prompt))
-                if value < 0 :
-                    raise ValueError("Error! Value must be a positive integer.")
+            value = input(prompt)
+            if value.isdigit and len(value) == 10 and int(value) >= 0 :
+                    return int(value)
             else:
-                value = float(input(prompt))
-                if value < 0:
-                    raise ValueError("Error! Value must be a positive number.")
-            return value
+                raise ValueError("Error! Value must be a positive 10-digit number")
         except ValueError:
-            print("Error! Invalid input. Please enter a positive {}.".format("integer" if is_integer else "number"))
-
+            print("Error! Invalid input. Please enter a positive 10-digit number")
 
 #Ini adalah penjabaran dari fungsi validasi angka 0-100 yang digunakan untuk input nilai siswa
 def validation_score(prompt):
@@ -197,27 +263,40 @@ def calculate_pass_status(math_score, science_score, english_score):
     average_score = (math_score + science_score + english_score) / 3
     return "Pass" if average_score >= 70 else "Fail"
 
-# Fungsi program menu utama
+#Ini adalah fungsi validasi agar nama tidak bisa dikosongkan dan harus diisi setidaknya dengan 1 karakter
+def validation_name(prompt):
+    while True:
+        try:
+            name = input(prompt).strip()
+            if len(name) > 0:
+                return name
+            else:
+                raise ValueError("Error! Must input at least 1 character.")
+        except ValueError as i:
+            print(i)
+
+
+# Fungsi program MAIN
 load_database()
 
 while True:
     display_menu()
-    choice = input("Enter your choice: ")
+    choice = input("Enter your choice (1-5): ")
 
     if choice == '1':
         read_database()
     elif choice == '2':
-        add_record()
+        add_submenu()
         save_database()
     elif choice == '3':
-        update_record()
+        update_submenu()
         save_database()
     elif choice == '4':
-        delete_record()
+        delete_submenu()
         save_database()
     elif choice == '5':
         print('''Thank you for using this program!     
-Programmed by : Muhammad Hunafa Rizal Aqli''')
+Programmed by : Muhammad Hunafa Rizal Aqli ^_^''')
         break
     else:
         print("Error! Please choose a number between 1-5")
---
+++ Readme.md
@@ -0,0 +1,22 @@
+## I'm sorry, Perwadhoka what??
+
+"Perwadhoka High School Score Database" is a program developed by a Data Scientist wannabe, Aqli. The purpose is to help schools store their student's data(s) on a more compacted environment. The reason why the programmer chose python languange is for its flexibility, lightweight program compared to others (e.g. Microsoft Excel) and is one of the simplest form of programming language compared to some others. This program offers four features of CRUD (create, read, update, delete) :
+
+1. **Read records**, This program has 2 modes in reading. It could display the entire data that exists in the database, OR it can show a specific data the user want to display depending on the student's ID searched for.
+2. **Add records**, The add record functions as a feature to create new data(s) that can be done through running the program itself instead of editing the database file.
+3. **Update records**, This function enables user to modify and manipulate any specified student's data to its desired display output.
+4. **Delete records**, By using delete record feature, user can completely remove any data existing on the database if user wished to do so.
+
+## Project Structure
+
+The directory structure of this Capstone project looks like this:
+
+    ├── README.md                <- A brief explanation regarding the Capstone Project.
+    │
+    ├── student_database.txt     <- Database for the program
+    │
+    ├── docs                     <- Presentation and relevant documents
+    │
+    ├── CC1.py                   <- Source code used in this project
+    │
+    └── Requirements.txt         <- The requirements file for reproducing the analysis environment, e.g.
